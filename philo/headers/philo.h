6f5c881b4b6b7f3b77a537bc7f7aee8532e91a7a
Pierre-marie LAM <plam@e2r1p5.clusters.42paris.fr>
2022-02-16 14:56:54 +0100

    starting the philosophers project

--- philo/headers/philo.h
+++ philo/headers/philo.h
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 16:59:38 by plam              #+#    #+#             */
-/*   Updated: 2022/02/16 14:22:34 by plam             ###   ########.fr       */
+/*   Updated: 2022/02/16 14:41:23 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ typedef	struct	s_philosopher
 	int				lifetime;
 	int				hungry;
 	int				meal_nb;
-	pthread_mutex_t	fork;
+	pthread_mutex_t	l_fork;
+	pthread_mutex_t	r_fork;
 }				t_philo;
 
 void	philo(int number_of_philosophers, int time_to_die, int time_to_sleep);
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 16:58:48 by plam              #+#    #+#             */
-/*   Updated: 2022/02/16 13:58:57 by plam             ###   ########.fr       */
+/*   Updated: 2022/02/16 14:56:50 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,12 @@ void	philo(int number_of_philosophers, int time_to_die, int time_to_sleep)
 
 int	main(int ac, char **av)
 {
+	if (ac == 4)
+	{
+		while (philo_nb() == ft_atoi(av[2]))
+		{
+			
+		}
+	}
 	return (0);
 }
