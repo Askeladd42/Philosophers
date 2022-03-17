d950b66b042b25c7aea6f305769c45adbd9c219b
plam <plam@student.42.fr>
2022-03-17 16:45:15 +0100

    added rules, table & parsing functions

---
+++ philosophers/srcs/time/ms_sleep.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_sleep.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 16:38:01 by plam              #+#    #+#             */
+/*   Updated: 2022/03/17 16:45:12 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ms_sleep(t_ms waiting_time, t_rules *rules)
+{
+	struct timeval	time_now;
+	t_ms			stop_time;
+	int				cnt;
+
+	cnt = 0;
+	stop_time = get_time(time_now) + waiting_time;
+	while (get_time(time_now) - stop_time < 0)
+	{
+		cnt++;
+		if (cnt == 5000)
+		{
+			cnt = 0;
+		}
+		usleep(100);
+		gettimeofday(&time_now, NULL);
+	}
+}
--- philosophers/srcs/time/set_time.c
+++ philosophers/srcs/time/set_time.c
@@ -6,16 +6,25 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 14:57:03 by plam              #+#    #+#             */
-/*   Updated: 2022/03/15 14:59:10 by plam             ###   ########.fr       */
+/*   Updated: 2022/03/17 16:37:11 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-long long	set_time(void)
-{
-	struct timeval	time;
+/*
+** Conversion time : first one is given in microseconds, so we have to * 1000 to
+** have it in ms (milliseconds), when the second one is in seconds, we have to
+** "dividing" it by 1000 to have it in ms.
+*/
 
+t_ms	set_time(struct timeval	time)
+{
 	gettimeofday(&time, NULL);
-	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000.0);
+}
+
+t_ms	get_diff_time(struct timeval begin, struct timeval end)
+{
+	return (set_time(end) - set_time(begin));
 }
