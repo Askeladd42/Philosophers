dfc22e4c8ea11862a97c7a79087ed5f5f29ee09a
plam <plam@student.42.fr>
2022-03-19 09:49:50 +0100

    starting the routine functions

---
+++ philosophers/srcs/routine/print_funcs.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_funcs.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/19 09:47:54 by plam              #+#    #+#             */
+/*   Updated: 2022/03/19 09:49:46 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*
+** Here we will deal with printing and changing status of threads during the
+** execution of them
+** Need to add a e_num or define status on the header
+*/
+
+void	change_status(t_philo *philo, int status)
+{
+	
+}
