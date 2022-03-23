a30a3f55b5684ea8737906fddcf1a5a2c5117380
Askeladd42 <piema.lam@gmail.com>
2022-03-23 14:47:43 +0100

    main reconfigured

--- philosophers/headers/philo_utils.h
+++ philosophers/headers/philo_utils.h
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 13:34:13 by plam              #+#    #+#             */
-/*   Updated: 2022/03/15 14:38:05 by plam             ###   ########.fr       */
+/*   Updated: 2022/03/23 14:45:56 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 
 # define LONG_MAX 2147483647
 
+# include <stddef.h>
+
 int		ft_isdigit(int c);
 void	*ft_memset(void *s, int c, size_t n);
 long	ft_atol(const char *str);
+
+#endif
